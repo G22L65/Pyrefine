932c9cc60f488e1a9e12ac2c1fd893230d1e053c
Arshad Ali <arshadali04042004@gmail.com>
2025-09-08 11:34:17 +0530

    api added

--- ai.py
+++ ai.py
@@ -1,12 +1,15 @@
 from google import genai
-
-client = genai.Client(api_key="AIzaSyDvRsAzTW2M2lAxMc5FQhZbKRRLeFzvYRM")
+from dotenv import load_dotenv
+import os
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
 
 def chat_with_gemini(user_input: str) -> str:
     
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=user_input + " You are a python code optimizer. For any code given to you, first calculate its complexity and then write another code with efficient complexity. Only provide Optimized Code without '''."
+        contents=user_input 
     )
     
     # ✅ Extract text from response (instead of returning raw object)
@@ -14,11 +17,11 @@ def chat_with_gemini(user_input: str) -> str:
         return response.text  # shortcut (Google Gemini SDK exposes .text)
     except AttributeError:
         if response.candidates and response.candidates[0].content.parts:
-            return response.candidates[0].content.parts[0].text
+            return response.candidates[0].content.parts[0].text.strip()
         return "⚠️ No response from Gemini."
     
     # cleaned = re.sub(r"```(?:python)?\n?|```", "", text).strip()
     # return cleaned
 
-# res = chat_with_gemini("code for febo")
-# print(res)
+# # res = chat_with_gemini("code for febo")
+# # print(res)
