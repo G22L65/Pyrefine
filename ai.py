89eb26a157e625e54b185460e81fb9aa47ebc9b8
Arshad Ali <arshadali04042004@gmail.com>
2025-09-08 11:43:12 +0530

    hardcoded again

--- ai.py
+++ ai.py
@@ -1,9 +1,8 @@
 from google import genai
-from dotenv import load_dotenv
-import os
-load_dotenv()
-api_key = os.getenv("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+
+
+
+client = genai.Client(api_key="AIzaSyDvRsAzTW2M2lAxMc5FQhZbKRRLeFzvYRM")
 
 def chat_with_gemini(user_input: str) -> str:
     
